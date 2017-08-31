b946f9e9445b7226191fbdc23d364e8a54b03468
Booom3 <joakim241@hotmail.com>
2017-08-31 16:15:05 +0200

    WIP

--- source/discord/discordapp.ts
+++ source/discord/discordapp.ts
@@ -7,9 +7,24 @@ client.on('ready', () => {
     debug('Bot is ready.');
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
     if (message.content === 'ping') {
         message.channel.send('pong');
+    }
+
+    if (message.content[0] === '!') {
+        let command = message.content.split(' ')[0].replace('!', '');
+        const { rows } = await db.query('SELECT response FROM commands WHERE guildid = $1 AND command = $2', [message.guild.id, command]);
+        if (rows.length === 0) {
+            return;
+        }
+        let response = rows[0].response;
+        if (response.type === "randomstring") {
+            let res = response.strings[Math.floor(Math.random() * response.strings.length)].string;
+            debug(command + ' used, sending response: ' + res);
+            message.channel.send(res);
+            return;
+        }
     }
 });
 
